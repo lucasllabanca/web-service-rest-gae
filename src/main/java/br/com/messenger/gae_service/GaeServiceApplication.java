0d460a56fd0ea867a4316d78848cabc7e3d51ad5
lucasllabanca <lucasllabanca@hotmail.com>
2022-03-16 23:58:51 -0300

    Initial config of java spring boot maven project

---
+++ src/main/java/br/com/messenger/gae_service/GaeServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.messenger.gae_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/messenger/gae_service/GaeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.messenger.gae_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
